ee00433df04d5618a72ef3f32eeeaa531d4da3c6
lesleytomosada <tomosadalesley@gmail.com>
2024-07-15 12:21:33 -0700

    completed get products by id

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -52,10 +52,11 @@ public class CategoriesController
     // the url to return all products in category 1 would look like this
     // https://localhost:8080/categories/1/products
     @GetMapping("{categoryId}/products")
-    public List<Product> getProductsById(@PathVariable int categoryId)
+    public ResponseEntity<List<Product>> getProductsById(@PathVariable int categoryId)
     {
         // get a list of product by categoryId
-        return null;
+        var products = productDao.listByCategoryId(categoryId);
+        return new ResponseEntity<>(products, HttpStatus.OK);
     }
 
     // add annotation to call this method for a POST action

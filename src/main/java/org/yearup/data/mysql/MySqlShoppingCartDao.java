f71ccc00d4e8773fba59989aa5ccc71080a80f50
McPherson <joanna.mcpherson@cvent.com>
2024-07-16 15:49:33 -0400

    cart1: draft

---
+++ src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
@@ -0,0 +1,62 @@
+package org.yearup.data.mysql;
+
+import org.springframework.stereotype.Component;
+import org.yearup.data.ProductDao;
+import org.yearup.data.ShoppingCartDao;
+import org.yearup.models.ShoppingCart;
+import org.yearup.models.Product;
+import org.yearup.models.ShoppingCartItem;
+
+import javax.sql.DataSource;
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+@Component
+public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDao {
+    private final ProductDao productDao;
+
+    public MySqlShoppingCartDao(DataSource dataSource, ProductDao productDao) {
+        super(dataSource);
+        this.productDao = productDao;
+    }
+
+    @Override
+    public ShoppingCart getByUserId(int userId) {
+        String sql = "SELECT * FROM shopping_cart WHERE user_id = ?";
+        ShoppingCart cart = null;
+
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setInt(1, userId);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    cart = mapRow(resultSet);
+                }
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return cart;
+    }
+
+    private ShoppingCart mapRow(ResultSet row) throws SQLException {
+        int userId = row.getInt("user_id");
+        int productId = row.getInt("product_id");
+        int quantity = row.getInt("quantity");
+        Product product = productDao.getById(productId);
+        ShoppingCartItem cartItem = new ShoppingCartItem();
+        cartItem.setProduct(product);
+        cartItem.setQuantity(quantity);
+        cartItem.setDiscountPercent(BigDecimal.valueOf(10));
+        ShoppingCart cart = new ShoppingCart();
+        cart.add(cartItem);
+        return cart;
+    }
+}

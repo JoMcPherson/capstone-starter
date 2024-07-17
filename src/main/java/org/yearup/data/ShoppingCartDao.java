94754a0d804de48a3b91fd3cd71c154604b06f6d
jared.harvick <jared.harvick@cvent.com>
2024-07-17 14:28:46 -0500

    Added addProductToCart method

--- src/main/java/org/yearup/data/ShoppingCartDao.java
+++ src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -6,4 +6,5 @@ public interface ShoppingCartDao
 {
     ShoppingCart getByUserId(int userId);
     // add additional method signatures here
+    void addProductToCart(ShoppingCart cart, int userId, int addedProductId);
 }
